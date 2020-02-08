0698bcf27fd97d04c30f91a99e6dac73a497730e
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-02-08 09:41:18 +0200

    Update config.php

--- config.php
+++ config.php
@@ -36,4 +36,21 @@ $fb = new Facebook(array(
     'app_secret' => FB_APP_SECRET, 
     'default_graph_version' => 'v5.x', 
 )); 
+
+// Get redirect login helper 
+$helper = $fb->getRedirectLoginHelper(); 
  
+// Try to get access token 
+try { 
+    if(isset($_SESSION['facebook_access_token'])){ 
+        $accessToken = $_SESSION['facebook_access_token']; 
+    }else{ 
+          $accessToken = $helper->getAccessToken(); 
+    } 
+} catch(FacebookResponseException $e) { 
+     echo 'Graph returned an error: ' . $e->getMessage(); 
+      exit; 
+} catch(FacebookSDKException $e) { 
+    echo 'Facebook SDK returned an error: ' . $e->getMessage(); 
+      exit; 
+}
