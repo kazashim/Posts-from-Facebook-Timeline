f8e019ba331f4280db2e41a31cc0a4a98ab8b659
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-02-08 09:43:56 +0200

    Update User.class.php

--- User.class.php
+++ User.class.php
@@ -23,3 +23,27 @@ class User {
             } 
         } 
     } 
+
+    function checkUser($userData = array()){ 
+        if(!empty($userData)){ 
+            // Check whether user data already exists in database 
+            $prevQuery = "SELECT * FROM ".$this->userTbl." WHERE oauth_provider = '".$userData['oauth_provider']."' AND oauth_uid = '".$userData['oauth_uid']."'"; 
+            $prevResult = $this->db->query($prevQuery); 
+            if($prevResult->num_rows > 0){ 
+                // Update user data if already exists 
+                $query = "UPDATE ".$this->userTbl." SET first_name = '".$userData['first_name']."', last_name = '".$userData['last_name']."', email = '".$userData['email']."', gender = '".$userData['gender']."', picture = '".$userData['picture']."', link = '".$userData['link']."', modified = NOW() WHERE oauth_provider = '".$userData['oauth_provider']."' AND oauth_uid = '".$userData['oauth_uid']."'"; 
+                $update = $this->db->query($query); 
+            }else{ 
+                // Insert user data 
+                $query = "INSERT INTO ".$this->userTbl." SET oauth_provider = '".$userData['oauth_provider']."', oauth_uid = '".$userData['oauth_uid']."', first_name = '".$userData['first_name']."', last_name = '".$userData['last_name']."', email = '".$userData['email']."', gender = '".$userData['gender']."', picture = '".$userData['picture']."', link = '".$userData['link']."', created = NOW(), modified = NOW()"; 
+                $insert = $this->db->query($query); 
+            } 
+             
+            // Get user data from the database 
+            $result = $this->db->query($prevQuery); 
+            $userData = $result->fetch_assoc(); 
+        } 
+         
+        // Return user data 
+        return $userData; 
+    } 
