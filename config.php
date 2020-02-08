1c7fbc05e296e85d05cfab025ca4da8c3905cd63
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-02-08 10:08:36 +0200

    Update config.php

--- config.php
+++ config.php
@@ -12,9 +12,9 @@ define('DB_USER_TBL', 'users');
 define('DB_POST_TBL', 'user_posts'); 
  
 // Facebook API configuration 
-define('FB_APP_ID', 'Insert_Facebook_App_ID'); // Replace {app-id} with your app id 
-define('FB_APP_SECRET', 'Insert_Facebook_App_Secret'); // Replace {app-secret} with your app secret 
-define('FB_REDIRECT_URL', 'Callback_URL');  
+define('FB_APP_ID', ''); // Replace {app-id} with your app id 
+define('FB_APP_SECRET', ''); // Replace {app-secret} with your app secret 
+define('FB_REDIRECT_URL', 'http://localhost/facebimg/');  
 define('FB_POST_LIMIT', 10); 
  
 // Start session 
